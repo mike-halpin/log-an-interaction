d158cfbf478681220807f8f1276f3fb9ef2a58cc
Michael Halpin <g+ithub@halpin.io>
2023-05-15 12:49:03 -0400

    Updating LWC to autolaunch

--- force-app/main/default/lwc/logInteractionButton/logInteractionButton.js
+++ force-app/main/default/lwc/logInteractionButton/logInteractionButton.js
@@ -1,14 +1,18 @@
-import { LightningElement } from 'lwc';
-import { NavigationMixin } from 'lightning/navigation';
+import { LightningElement } from "lwc";
+import { NavigationMixin } from "lightning/navigation";
 
 export default class LaunchFlow extends NavigationMixin(LightningElement) {
-    startFlow() {
-        // Navigate to the flow
-        this[NavigationMixin.Navigate]({
-            type: 'standard__webPage',
-            attributes: {
-                url: '/flow/LogInteractionSaga'
-            }
-        });
-    }
+  connectedCallback() {
+    this.startFlow();
+  }
+
+  startFlow() {
+    // Navigate to the flow
+    this[NavigationMixin.Navigate]({
+      type: "standard__webPage",
+      attributes: {
+        url: "/flow/LogInteractionSaga"
+      }
+    });
+  }
 }
